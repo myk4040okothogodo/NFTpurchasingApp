1107f1a06e59b7d5b5440a423c38afb0378e666b
myk4040okothogodo <mikeogodo5@gmail.com>
2022-04-08 12:20:00 +0300

    inception

---
+++ screens/Home.js
@@ -0,0 +1,4 @@
+import React, {useState} from 'react';
+import { View, SafeAreaView, FlatList } from "react-native";
+import { NFTCard, HomeHeader, FocusedStatusBar} from "../components";
+import { COLORS, NFTData } from "../constants";
